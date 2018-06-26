1108077ad5d6fdaf2539511fbd78fd584cea32b2
tnnyang <2275182329@qq.com>
2018-06-26 10:24:02 +0800

    添加兼容IE的插件

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -12,9 +12,10 @@ function resolve (dir) {
 
 module.exports = {
   context: path.resolve(__dirname, '../'),
-  entry: {
-    app: './src/main.js'
-  },
+  // entry: {
+  //   app: './src/main.js'
+  // },
+  entry: ["babel-polyfill", "./src/main.js"],    //兼容IE
   output: {
     path: config.build.assetsRoot,
     filename: '[name].js',
--- src/main.js
+++ src/main.js
@@ -3,10 +3,9 @@ import App from './App'
 import router from './router'
 import axios from 'axios'
 import store from './vuex'
-import './assets/bootstrap/css/bootstrap.min.css'
-import './assets/css/account.css'
-import 'nprogress/nprogress.css'   //路由切换进度条样式
 import * as util from './util/util'
+
+import 'babel-polyfill'    //兼容IE
 
 Vue.config.productionTip = false
 
